b09164c8a4e48090285723dd33064cc08044b080
Starinin Andrey <starininandrey@gmail.com>
2017-08-07 15:14:45 +0300

    Загрузка проекта
    Добавление файлов в репозитарий

---
+++ RestartSpooler.ps1
@@ -0,0 +1,3 @@
+﻿Remove-Item -Path "C:\Windows\system32\spool\printers\*.spl" -Recurse -Force -ErrorAction SilentlyContinue -Verbose
+Remove-Item -Path "C:\Windows\system32\spool\printers\*.shd" -Recurse -Force -ErrorAction SilentlyContinue -Verbose
+Restart-Service spooler -PassThru
